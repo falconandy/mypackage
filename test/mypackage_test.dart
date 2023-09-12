5f33bdfa27d7f0731315ba9bbbc9354381a133c6
Andrey Sokolov <andrey.sokolov@amplitude.com>
2023-09-12 20:49:07 +0400

    fix: update package name in tests

--- test/mypackage_test.dart
+++ test/mypackage_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mypackage/mypackage.dart';
+import 'package:falconandy_mypackage/mypackage.dart';
 
 void main() {
   test('adds one to input values', () {
