b9d9c165af2d3807886d0f900bfd6be4be6dfd2b
Andrey Sokolov <andrey.sokolov@amplitude.com>
2023-09-12 19:31:04 +0400

    fix: updated logic

--- lib/mypackage.dart
+++ lib/mypackage.dart
@@ -2,6 +2,6 @@ library mypackage;
 
 /// A Calculator.
 class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+  /// Returns [value] plus 2.
+  int addOne(int value) => value + 2;
 }
--- test/mypackage_test.dart
+++ test/mypackage_test.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mypackage/mypackage.dart';
+import 'package:falconandy_mypackage/mypackage.dart';
 
 void main() {
   test('adds one to input values', () {
     final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+    expect(calculator.addOne(2), 4);
+    expect(calculator.addOne(-7), -5);
+    expect(calculator.addOne(0), 2);
   });
 }
